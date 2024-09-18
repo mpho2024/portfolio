718312fe6c7c67d8c71f18f32c9cbd578d80d9bc
mpho2024 <mpho.tsatsi01@gmail.com>
2024-09-18 07:19:42 +0200

    projects

--- mpho.portfolio/src/app/page.js
+++ mpho.portfolio/src/app/page.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectSection";
 import EmailSection from "./components/EmailSection";
+
 
 export default function Home() {
   return (
@@ -12,8 +14,13 @@ export default function Home() {
     <div className="container mt-24 mx-auto px-12 py-4">
       <AboutSection/>
       </div>
+      <section id="project">
+      <ProjectsSection/>
+      </section>
+
       <section id="contact">
       <EmailSection/>
+    
       </section>
   
     </main>
